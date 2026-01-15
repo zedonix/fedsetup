9945ccce0f10cd2ea067820058cab252f007fcfb
zedonarch <zedonix@proton.me>
2026-01-15 07:59:26 +0530

    hdf

--- install.sh
+++ install.sh
@@ -66,7 +66,7 @@ gpgcheck=True
 EOF
 dnf clean all
 dnf makecache
-dnf upgrade -y --refresh
+dnf upgrade -y --refresh --exclude=gstreamer1-plugin-openh264
 ## Adding repos
 dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-"$(rpm -E %fedora)".noarch.rpm
 dnf install -y https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-"$(rpm -E %fedora)".noarch.rpm
